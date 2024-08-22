db7e358318095b75a17e6e11e54168b33736e2d8
germanreynoso <germanreynoso94@gmail.com>
2024-08-22 04:44:03 -0300

    un paso en las migraciones

--- ecommerce-german-reynoso/src/config/data-source.ts
+++ ecommerce-german-reynoso/src/config/data-source.ts
@@ -1,33 +1,6 @@
-// import { DataSource, DataSourceOptions } from 'typeorm';
-// import * as dotenv from 'dotenv';
-// import { registerAs } from '@nestjs/config';
-
-// dotenv.config({
-//   path: '.env',
-// });
-
-// const PostgresDataSource: DataSourceOptions = {
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT, 10),
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   synchronize: true,
-//   logging: false,
-//   entities: ['dist/**/*.entity{.ts,.js}'],
-//   migrations: ['dist/migration/*{ts,.js}'],
-// };
-
-// export const PostgresDataSourceConfig = registerAs('postgres', () => PostgresDataSource);
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { registerAs } from '@nestjs/config';
-import { Order } from '../orders/entities/order.entity';
-import { OrderDetail } from '../order-details/entities/order-detail.entity';
-import { Product } from '../products/entities/product.entity';
-import { User } from '../users/entities/user.entity';
-import { Category } from '../categories/entities/category.entity';
 
 dotenv.config({
   path: '.env',
@@ -40,14 +13,14 @@ const PostgresDataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: false,
+  synchronize: true,
   logging: false,
-  entities: [Order, OrderDetail, Product, User, Category],
-  migrations: ['dist/migration/*{.ts,.js}'],
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migration/*{ts,.js}'],
 };
 
-// Exporta una instancia de DataSource para usar con TypeORM CLI
-export const PostgresDataSource = new DataSource(PostgresDataSourceOptions);
+export const PostgresDataSourceConfig = registerAs(
+  'postgres',
+   () => PostgresDataSourceOptions);
 
-// Exporta la configuración para usar con ConfigModule
-export const PostgresDataSourceConfig = registerAs('postgres', () => PostgresDataSourceOptions);
+export const PostgresDataSource = new DataSource(PostgresDataSourceOptions);
